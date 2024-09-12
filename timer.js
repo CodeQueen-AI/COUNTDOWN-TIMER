47766d130a89e1345483ff34c26834a86b405596
Sumbal Naz <Codeq209@gmail.com>
2024-09-12 16:17:26 +0500

    Add files via upload

---
+++ timer.js
@@ -0,0 +1,36 @@
+#! /usr/bin/env node
+import { differenceInSeconds } from "date-fns";
+import inquirer from "inquirer";
+const res = await inquirer.prompt({
+    type: "number",
+    name: "userInput",
+    message: "Please Enter the amount of second",
+    validate: (input) => {
+        if (isNaN(input)) {
+            return "enter the number";
+        }
+        else if (input > 60) {
+            return "Seconds must be in 60";
+        }
+        else {
+            return true;
+        }
+    }
+});
+let input = res.userInput;
+function startTimer(val) {
+    const intTime = new Date().setSeconds(new Date().getSeconds() + val);
+    const intervalTime = new Date(intTime);
+    setInterval(() => {
+        const currentTime = new Date();
+        const timeDiff = differenceInSeconds(intervalTime, currentTime);
+        if (timeDiff <= 0) {
+            console.log('Time has expired');
+            process.exit();
+        }
+        const min = Math.floor((timeDiff % (3600 * 24)) / 3600);
+        const sec = Math.floor(timeDiff % 60);
+        console.log(`${min.toString().padStart(2, "0")}:${sec.toString().padStart(2, "0")}`);
+    }), 1000;
+}
+startTimer(input);
---
+++ timer.ts
@@ -0,0 +1,38 @@
+#! /usr/bin/env node
+
+import { differenceInSeconds, min } from "date-fns";
+import inquirer from "inquirer";
+const res = await inquirer.prompt({
+    type: "number",
+    name:"userInput",
+    message: "Please Enter the amount of second",
+    validate : (input) =>{
+        if(isNaN(input)){
+            return "enter the number"
+        }else if(input > 60) {
+            return "Seconds must be in 60"
+        }else {
+            return true;
+        }
+    }
+})
+let input = res.userInput
+function startTimer(val:number){
+    const intTime = new Date().setSeconds(new Date().getSeconds() + val)
+const intervalTime = new Date(intTime)
+   setInterval(()=>{
+    const currentTime = new Date()
+    const timeDiff= differenceInSeconds(intervalTime,currentTime)
+    if(timeDiff <= 0){
+        console.log('Time has expired')
+        process.exit()
+    }
+    const min = Math.floor((timeDiff%(3600*24))/3600)
+    const sec = Math.floor(timeDiff% 60);
+    console.log(`${min.toString().padStart(2, "0")}:${sec.toString().padStart(2, "0")}`);
+   }), 1000
+}
+startTimer(input)
+
+
+
